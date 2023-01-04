da4c8949847b615c69c38de9bf1ecbb2da5c29c8
Pitlivka <88449521+Pitlivka@users.noreply.github.com>
2023-01-04 18:06:20 +0000

    Game functioning.
    Missing:
    king and ace combination
    5 cards
    
    gui and notes

--- test/test_deck.py
+++ test/test_deck.py
@@ -1,18 +1,20 @@
 import unittest
-from src.deck import Deck
+from src.blackjack import Blackjack
 
 
 class DeckTestCase(unittest.TestCase):
 
     def setUp(self):  # this method will be run before each test
-        self.deck = Deck()
+        self.blackjack = Blackjack()
 
     def tearDown(self):  # this method will be run after each tests
         pass
 
     def test_number_of_cards(self):  # any method beginning with 'test' will be run by unittest
-        number_of_cards = len(self.deck.cards)
-        self.assertEqual(number_of_cards, 52)
+        number_of_cards = len(self.blackjack.cards)
+        self.assertEqual(number_of_cards, 56)
+
+
 
 
 if __name__ == '__main__':
